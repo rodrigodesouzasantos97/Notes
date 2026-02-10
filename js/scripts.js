9d93a4ee595e375127ded5942e9bc880e0a25178
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-02-10 14:49:32 -0300

    Corrections in HTML/CSS and Created functions in JS
    Functions: add note, delete note, duplicate note, fix note, search note and export note in CSV.

---
+++ js/scripts.js
@@ -0,0 +1,252 @@
+const addNoteTitleInput = document.querySelector("#add-note-title");
+const addNoteDescriptionInput = document.querySelector("#add-note-description");
+const addNoteBtn = document.querySelector("#add-note-btn");
+const notesContainer = document.querySelector("#notes-container");
+const searchInput = document.querySelector("#search-container input");
+const searchBtn = document.querySelector("#search-btn");
+const exportCsvBtn = document.querySelector("#export-csv")
+
+let wentInCreateNote = false;
+
+function showNotes() {
+    cleanNotes();
+
+    getNotes().forEach(note => {
+        const noteElement = createNote(note.id, note.title, note.description, note.fixed);
+        notesContainer.appendChild(noteElement);
+    });
+}
+
+function cleanNotes() {
+    notesContainer.replaceChildren([]);
+}
+
+function addNote() {
+    const titleIsNullOrEmpty = !addNoteTitleInput.value.trim();
+    const descriptionIsNullOrEmpty = !addNoteDescriptionInput.value.trim();
+
+    wentInCreateNote = false;
+
+    if (titleIsNullOrEmpty || descriptionIsNullOrEmpty) return;
+
+    wentInCreateNote = true;
+
+    const notes = getNotes();
+
+    const noteObject = {
+        id: generateId(),
+        title: addNoteTitleInput.value,
+        description: addNoteDescriptionInput.value,
+        fixed: false
+    }
+
+    const noteElement = createNote(noteObject.id, noteObject.title, noteObject.description);
+    notesContainer.appendChild(noteElement);
+
+    notes.push(noteObject);
+    saveNotes(notes);
+
+    addNoteTitleInput.value = "";
+    addNoteDescriptionInput.value = "";
+
+    if (!addNoteTitleInput.classList.contains("hide")) {
+        addNoteTitleInput.classList.add("hide");
+    }
+}
+
+function generateId() {
+    return Math.floor(Math.random() * 5000);
+}
+
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+    const orderedNotes = notes.sort((a, b) => a.fixed > b.fixed ? -1 : 1);
+
+    return orderedNotes;
+}
+
+function saveNotes(notes) {
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function createNote(id, title, description, fixed) {
+    const divNote = document.createElement("div");
+    divNote.classList.add("note");
+
+    const h2 = document.createElement("h2");
+    divNote.appendChild(h2);
+    h2.innerText = title;
+
+    const p = document.createElement("p");
+    divNote.appendChild(p);
+    p.innerText = description;
+
+    const divBtns = document.createElement("div");
+    divBtns.classList.add("note-btns");
+    divNote.appendChild(divBtns);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    const trashCanIcon = document.createElement("i");
+    trashCanIcon.classList.add(...["fa-solid", "fa-trash-can"]);
+    deleteBtn.appendChild(trashCanIcon);
+    divBtns.appendChild(deleteBtn);
+
+    const duplicateBtn = document.createElement("button");
+    duplicateBtn.classList.add("duplicate-btn");
+    const copyIcon = document.createElement("i");
+    copyIcon.classList.add(...["fa-solid", "fa-copy"]);
+    duplicateBtn.appendChild(copyIcon);
+    divBtns.appendChild(duplicateBtn);
+
+    const fixBtn = document.createElement("button");
+    fixBtn.classList.add("fix-btn");
+    const thumbtackIcon = document.createElement("i");
+    thumbtackIcon.classList.add(...["fa-solid", "fa-thumbtack"]);
+    fixBtn.appendChild(thumbtackIcon);
+    const thumbtackSlashIcon = document.createElement("i");
+    thumbtackSlashIcon.classList.add(...["fa-solid", "fa-thumbtack-slash", "hide"]);
+    fixBtn.appendChild(thumbtackSlashIcon);
+    divBtns.appendChild(fixBtn);
+
+    deleteBtn.addEventListener("click", () => {
+        deleteNote(id, divNote);
+    });
+
+    duplicateBtn.addEventListener("click", () => {
+        duplicateNote(id);
+    });
+
+    if (fixed) {
+        divNote.classList.add("fixed")
+        thumbtackIcon.classList.add("hide");
+        thumbtackSlashIcon.classList.remove("hide");
+    }
+
+    fixBtn.addEventListener("click", () => {
+        toggleFixBtn(id);
+    });
+
+    return divNote;
+}
+
+function toggleFixBtn(id) {
+    const notes = getNotes();
+
+    const targetNote = notes.filter((note) => note.id === id)[0];
+    targetNote.fixed = !targetNote.fixed;
+
+    saveNotes(notes);
+
+    showNotes();
+}
+
+function deleteNote(id, element) {
+    const notes = getNotes();
+
+    const filteredNotes = notes.filter((note) => note.id !== id);
+
+    saveNotes(filteredNotes);
+
+    notesContainer.removeChild(element);
+}
+
+function duplicateNote(id) {
+    const notes = getNotes();
+
+    const noteTarget = notes.filter((note) => note.id === id)[0];
+
+    const noteObject = {
+        id: generateId(),
+        title: noteTarget.title,
+        description: noteTarget.description,
+        fixed: false
+    }
+
+    const noteElement = createNote(noteObject.id, noteObject.title, noteObject.description);
+    notesContainer.appendChild(noteElement);
+
+    notes.push(noteObject);
+    saveNotes(notes);
+}
+
+function toggleAddNoteTitle(id) {
+    if (
+        id === "add-note-form-inputs" ||
+        id === "add-note-title" ||
+        id === "add-note-description" ||
+        (id === "add-note-btn" && wentInCreateNote === false)
+    ) {
+        if (addNoteTitleInput.classList.contains("hide")) {
+            addNoteTitleInput.classList.remove("hide");
+        }
+    } else {
+        if (!addNoteTitleInput.classList.contains("hide")) {
+            addNoteTitleInput.classList.add("hide");
+            addNoteTitleInput.value = "";
+            addNoteDescriptionInput.value = "";
+        }
+    }
+}
+
+function searchValue(value) {
+    const notes = getNotes().filter((note) => note.title.includes(value));
+
+    const searchInputIsNullOrEmpty = !searchInput.value.trim();
+
+    if (!searchInputIsNullOrEmpty) {
+        cleanNotes();
+
+        notes.forEach((note) => {
+            const noteElement = createNote(note.id, note.title, note.description, note.fixed);
+            notesContainer.appendChild(noteElement);
+        });
+
+        return;
+    }
+
+    showNotes();
+}
+
+function exportData() {
+    const notes = getNotes();
+
+    const csvString = [
+        ["ID", "Título", "Descrição", "Fixado?"],
+        ...notes.map((note) => [note.id, note.title, note.description, note.fixed])
+    ].map((e) => e.join(",")).join("\n");
+
+    const element = document.createElement("a")
+    element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+    element.target = "_blank"
+    element.download = "notes.csv"
+    element.click();
+}
+
+document.addEventListener("click", (e) => {
+    const elementId = e.target.id;
+    toggleAddNoteTitle(elementId);
+});
+
+addNoteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addNote();
+});
+
+searchInput.addEventListener("keyup", (e) => {
+    const value = e.target.value;
+    searchValue(value);
+});
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchInput.value = "";
+    searchInput.dispatchEvent(new Event("keyup"));
+});
+
+exportCsvBtn.addEventListener("click", () => {
+    exportData();
+});
+
+showNotes();
